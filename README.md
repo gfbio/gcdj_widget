da3db00dd3a80152679dec3684c3c9896cbcbfff
Marc Weber <maweber@mpi-bremen.de>
2014-08-22 09:42:26 +0200

    master adepting this repositories content to the current state of development
    - adept readme

--- README.md
+++ README.md
@@ -1,37 +1,75 @@
-# GCDJ Widget
-#### JavaScript based customizable view to create and edit json conform to GCDJ schemata
+# GCDJ Widget #
+### JavaScript based customizable view to create and edit json conform to GCDJ schemata ###
 
-##Installation
-- checkout or download
-- extract
-- move/copy to your webserver
-- include to html page (compare example.html)
 
-##Configuration
-On top of the main gcdj_widget.js file, certain configuration values have to be set
-in order to make the widget work on a server.
+## Installation ##
+- No explicit installation or download needed
+- Just include the required javascript and css
 
-* The name of the target container inside a *.html file (div is expected here)
-* An URI of a server-side script that handles the widgets submitted formdata
-* A location where to find check_list schema and options files (*.json)
-* Same for the select form in the widgets first step
+```
+#!html
 
-```javascript
-/******** CONFIGURATION ********/
-var targetContainerId = "target_1";
-var checkListFormAction = "TheAction";
-var checkListSchemaLocation = "./gcdj_widget/data/checklist_schema.json";
-var checkListOptionsLocation = "./gcdj_widget/data/checklist_options.json";
-var selectSchemaLocation = "./gcdj_widget/data/select_schema.json";
-var selectOptionsLocation = "./gcdj_widget/data/select_options.json";
-// add extra validation if you like
-function submitCheckListForm(form){
- return true;
-}
-/******** END CONFIGURATION *****/
+<!-- gcdj_widget css and javascript imports -->
+    <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-sliderAccess.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="http://www.alpacajs.org/js/alpaca-full.min.js"></script>
+    <script src="http://alni.mpi-bremen.de/static/gcdj_widget/js/gcdj_widget.js"></script>
+    <link rel="stylesheet" href="http://alni.mpi-bremen.de/static/gcdj_widget/css/gcdj_widget.css">
+<!-- end of gcdj_widget css and javascript imports -->
+
 ```
 
-##Libraries
-- Latest JQuery release as available via http://code.jquery.com/jquery-latest.js
-- jQuery Templates Plugin 1.0.0pre as available via http://github.com/jquery/jquery-tmpl (contained in widget package)
-- Alpaca Version 1.1.2 as available via http://www.alpacajs.org/web/download.html(contained in widget package
+- Add a target container into your HTML code
+
+```
+#!html
+<div id="target_1"></div>
+```
+
+
+
+## Configuration ##
+The following settings can be customized:
+
+1. Set the name of the target container inside a *.html file (div is expected here). This container will be populated with a variety of form fields.
+2. Set the url to the serverside componenent that will recieve the submitted form
+3. Implement a post render function to add custom code to the freshly rendered widget. E.g. add some logging or add some code to allow requests to your server-side code
+4. Choose to validate the json from the form against the "official" GCDJ validation webservice
+
+
+```
+#!html
+
+<script>
+   var targetContainerId = "target_1";
+   var checkListFormAction = "theAction";
+   var postRenderAction = function () {
+      console.log('postrender from html');
+   };
+   var validateUsingWebservice = true;
+</script>
+
+```
+
+## Libraries ##
+Right now GCDJ widget has dependencies to the following components (compare Installation section)
+
+### Jquery: ###
+* JQuery 2.1.1 release as available via http://code.jquery.com/jquery-2.1.1.min.js
+* Alpaca Version 1.1.3 as available via http://www.alpacajs.org/js/alpaca-full.min.js
+
+### JqueryUI: ###
+* JQueryUI 1.11.1 release as available via http://code.jquery.com/ui/1.11.1/jquery-ui.min.js
+* JQueryUI timepicker add-on as available via //cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js
+* Slider access for timepicker as available via //cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-sliderAccess.js
+
+### Boostrap: ###
+* Bootstap release  3.0.3 javascript as available via //netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"
+
+### GCDJ-Widget: ###
+* The widget javascript sources are delivered via http://alni.mpi-bremen.de/static/gcdj_widget/js/gcdj_widget.js
+* All CSS dependencies and imports are resolved by using the GCDJ-widget css file as available here  http://alni.mpi-bremen.de/static/gcdj_widget/css/gcdj_widget.css
+
--- gcdj_widget/js/gcdj_widget.js
+++ gcdj_widget/js/gcdj_widget.js
@@ -1,64 +1,177 @@
-/*
-* VERSION: 0.0.3
-* */
+/*!
+ GCDJ_WIDGET VERSION 0.0.7
+ */
 /********   CONFIGURATION  ********/
 
-var targetContainerId = "target_1";
-var checkListFormAction = "TheAction";
+var checkListSchemaLocation = 'http://alni.mpi-bremen.de/widget_schemas/checklist';
+var checkListOptionsLocation = 'http://alni.mpi-bremen.de/widget_options/checklist';
 
-var checkListSchemaLocation = "./gcdj_widget/data/checklist_schema.json";
-var checkListOptionsLocation = "./gcdj_widget/data/checklist_options.json";
-
-var selectSchemaLocation = "./gcdj_widget/data/select_schema.json";
-var selectOptionsLocation = "./gcdj_widget/data/select_options.json";
-
-// add extra validation if you like
-function submitCheckListForm(form){
-    return true;
-}
+var selectSchemaLocation = 'http://alni.mpi-bremen.de/widget_schemas/select';
+var selectOptionsLocation = 'http://alni.mpi-bremen.de/widget_options/select';
 
 /********   END CONFIGURATION  *****/
 
 var selectInputBundle = {
-    "optionsSource": selectOptionsLocation,
-    "schemaSource": selectSchemaLocation
+    'optionsSource': selectOptionsLocation,
+    'schemaSource': selectSchemaLocation
 };
 
 function renderSelectForm() {
-    $("#"+targetContainerId).alpaca(selectInputBundle);
+    $('#' + targetContainerId).alpaca(selectInputBundle);
 }
 
 var checkListOptions = {};
 var checkListSchema = {};
-var definitionsPrefix = "#/definitions/";
+var definitionsPrefix = '#/definitions/';
+var globalForm = {};
+var checklist = '';
+var pack = '';
 
-function renderCheckListForm() {
-    checkListOptions["form"]["attributes"]["action"] = checkListFormAction;
-    checkListSchema["options"] = checkListOptions;
-    checkListSchema["schema"] = checkListSchema;
+var boolean_fields = [];
 
-    $("#"+targetContainerId).alpaca(checkListSchema);
+var feedbackId = 'gcdj_feedback';
+var feedbackHeader = 'The validation of your data against the GCDJ Webservice resulted in following errors';
+var feedbackSucces = 'The validation of your data against the GCDJ Webservice found no errors. click again to submit';
 
+var formPassedValidation = false;
+
+function addJsonFormField(gcdjson) {
+    $('<input />').attr('type', 'hidden')
+        .attr('name', 'gcdjson')
+        .attr('value', JSON.stringify(gcdjson))
+        .appendTo('form#gcdj_checklist_form');
 }
 
-function submitSelectForm(form){
-    $("form#gcdj_select_form").submit(function(event) {
-        checkListSchema["properties"]["Environmental-Packages"]["$ref"] = definitionsPrefix+$("#Environmental-Packages").val();
-        checkListSchema["properties"]["MIxS-Checklists"]["$ref"] = definitionsPrefix+$("#MIxS-Checklists").val();
-        $("#"+targetContainerId).empty();
+function onSuccess(data) {
+    $('#' + feedbackId).empty();
+    if (data['gcdj_valid']) {
+        $('#' + feedbackId).css('marginLeft', '30px');
+        $('#' + feedbackId).append('<h4>' + feedbackSucces + '</h4>');
+        return true;
+    }
+    else {
+        $('#' + feedbackId).css('marginLeft', '30px');
+        $('#' + feedbackId).append('<h4>' + feedbackHeader + '</h4>');
+        $('#' + feedbackId).append('<ul>');
+        $.each(data['errors'], function (index, value) {
+            $('#' + feedbackId).append('<li>' + value + '</li>');
+        });
+        $('#' + feedbackId).append('</ul>');
+        return false;
+    }
+}
+
+function submitCheckListForm(form) {
+    return true;
+}
+
+function prepareFeedbackContainer() {
+    div = document.createElement('div');
+    div.id = feedbackId;
+    $('#' + targetContainerId).append(div);
+}
+
+var isValidationTriggerSet = function () {
+    return !(typeof validateUsingWebservice === 'undefined');
+};
+
+var useWebServiceValidation = function () {
+    if (isValidationTriggerSet()) {
+        return validateUsingWebservice;
+    }
+    else {
+        return true;
+    }
+};
+
+function add_missing_keys(gcdjson) {
+    for (var key in boolean_fields) {
+        if ($.inArray(boolean_fields[key], gcdjson) == -1) {
+            gcdjson[boolean_fields[key]] = false;
+        }
+    }
+}
+
+function renderCheckListForm() {
+    checkListOptions['form']['attributes']['action'] = checkListFormAction;
+    checkListSchema['options'] = checkListOptions;
+    checkListSchema['schema'] = checkListSchema;
+    checkListSchema['postRender'] = function (form) {
+        postRenderAction();
+        prepareFeedbackContainer();
+        globalForm = form;
+    };
+    $('#' + targetContainerId).alpaca(checkListSchema);
+
+    if (useWebServiceValidation()) {
+        $('#checklist_submit').click(function (event) {
+            if (!formPassedValidation) {
+                event.preventDefault();
+                var formJson = globalForm.getValue();
+                var gcdjson = $.extend({}, formJson['Environmental-Packages'],
+                    formJson['MIxS-Checklists']);
+
+                add_missing_keys(gcdjson);
+
+                var url = 'http://alni.mpi-bremen.de/validate?checklist=' + checklist + '&package=' + pack;
+                var jqxhr = $.post(url, {'gcdjson': JSON.stringify(gcdjson)},
+                    function (data) {
+                        formPassedValidation = onSuccess(data);
+                        if (formPassedValidation) {
+                            addJsonFormField(gcdjson);
+                        }
+                    })
+                    .fail(function () {
+                        console.log(' validateJsonViaWebService error ');
+                    })
+                    .always(function () {
+                        console.log(' validateJsonViaWebService finished valid = ' + formPassedValidation);
+                    });
+            }
+        });
+    }
+    else {
+        var formJson = globalForm.getValue();
+        var gcdjson = $.extend({}, formJson['Environmental-Packages'],
+            formJson['MIxS-Checklists']);
+        addJsonFormField(gcdjson);
+    }
+}
+
+function extract_boolean_fields(schema_properties) {
+    for (var key in schema_properties) {
+        if (schema_properties.hasOwnProperty(key)) {
+            if (schema_properties[key]['type'] == 'boolean') {
+                boolean_fields[boolean_fields.length] = key;
+            }
+        }
+    }
+}
+
+function submitSelectForm(form) {
+    $('form#gcdj_select_form').submit(function (event) {
+        pack = $('#Environmental-Packages').val();
+        checklist = $('#MIxS-Checklists').val();
+        checkListSchema['properties']['Environmental-Packages']['$ref'] = definitionsPrefix + pack;
+        checkListSchema['properties']['MIxS-Checklists']['$ref'] = definitionsPrefix + checklist;
+        $('#' + targetContainerId).empty();
+
+        extract_boolean_fields(checkListSchema['definitions'][checklist]['properties']);
+        extract_boolean_fields(checkListSchema['definitions'][pack]['properties']);
+
         renderCheckListForm();
         event.preventDefault();
     });
     return true;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $.when(
         $.getJSON(checkListOptionsLocation),
         $.getJSON(checkListSchemaLocation)
-    ).then(function(a, b){
+    ).then(function (a, b) {
             checkListOptions = a[0];
             checkListSchema = b[0];
             renderSelectForm();
-    });
+        });
 });
